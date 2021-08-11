c124e09db83839a1ec01cb47c27b0134837829a9
diromdp <dirom.purbowiseno@salt.co.id>
2021-08-11 23:43:34 +0700

    adwad
    :

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,7 +25,7 @@ export default function Home({ appName }) {
 
    const regexEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-   const _handleValidateEmail = () => {
+   const asdasd = () => {
       if (!email.value || !regexEmail.test(email.value)) {
          let errorMessage = "";
          if (!regexEmail.test(email.value)) {
